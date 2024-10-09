192de410b9fb64edfec24d76a3e74795e280594f
LangstonSparks <langston.sparks@hgs.hiddengeniusproject.org>
2024-10-08 18:52:59 -0700

    Add files via upload

---
+++ Form-Submission/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('myForm');
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+
+
+
+        if (name && email && message) {
+            alert("Form submitted successfully!\nName: ${name}\nEmail: ${email}\nMessage: ${message}");
+        } else {
+            alert("Please fill all the fields!");
+        }
+    });
+});
